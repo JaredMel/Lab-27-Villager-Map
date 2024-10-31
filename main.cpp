de7ac565f60331e2df54641351fafd7bb8ccf184
JaredMel <jareddmelendez1764@gmail.com>
2024-10-30 20:57:53 -0700

    WIP

--- main.cpp
+++ main.cpp
@@ -4,30 +4,74 @@
 #include <tuple>
 using namespace std;
 
+void addVillager(map<string, tuple<int, string, string>>&);
+void deleteVillager(map<string, tuple<int, string, string>>&);
+void increaseFriendship(map<string, tuple<int, string, string>>&);
+void decreaseFriendship(map<string, tuple<int, string, string>>&);
+void searchVillager(map<string, tuple<int, string, string>>&);
+
 int main() {
     // declarations
-    map<string, tuple<int, string, string>> villagerColors;
+    map<string, tuple<int, string, string>> villagers;
     int choice;
 
     // insert elements into the map
     // note how the right-hand side of the assignment are the vector elements
-    villagerColors["Drago"] = {5, "Alligator", "Snap to It!"};
-    villagerColors["Raymond"] = {8, "Cat", "Nice fit"};
-    villagerColors.insert({"Kyle", {10, "Wolf", "Hubba hubba!"}});
-
-    cout << "1. Add Villager" << endl;
-    cout << "2. Delete Villager" << endl;
-    cout << "3. Increase Friendship" << endl;
-    cout << "4. Decrease Friendship" << endl;
-    cout << "5. Search for Villager" << endl;
-    cout << "6. Exit" << endl;
-
-    switch ()
-    {
-    case /* constant-expression */:
-        /* code */
-        break;
+    villagers["Drago"] = {5, "Alligator", "Snap to It!"};
+    villagers["Raymond"] = {8, "Cat", "Nice fit"};
+    villagers.insert({"Kyle", {10, "Wolf", "Hubba hubba!"}});
     
+    while (choice != 6)
+    {
+        do
+        {
+            cout << "1. Add Villager" << endl;
+            cout << "2. Delete Villager" << endl;
+            cout << "3. Increase Friendship" << endl;
+            cout << "4. Decrease Friendship" << endl;
+            cout << "5. Search for Villager" << endl;
+            cout << "6. Exit" << endl;
+            cin >> choice;
+        } while (choice < 1 || choice > 6);
+        switch (choice)
+        {
+        case 1:
+            addVillager(villagers);
+            break;
+        case 2:
+        deleteVillager(villagers);
+        break;
+    case 3:
+        increaseFriendship(villagers);
+        break;
+    case 4:
+        decreaseFriendship(villagers);
+        break;
+    case 5:
+        searchVillager(villagers);
+        break;
+    default:
+        break;
+    }
+    }
+    
+    switch (choice)
+    {
+    case 1:
+        addVillager(villagers);
+        break;
+    case 2:
+        deleteVillager(villagers);
+        break;
+    case 3:
+        increaseFriendship(villagers);
+        break;
+    case 4:
+        decreaseFriendship(villagers);
+        break;
+    case 5:
+        searchVillager(villagers);
+        break;
     default:
         break;
     }
